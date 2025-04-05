c19b19666d342ea570b18a813561a0515f66ffd3
Nomad4202 <124133127+Nomad4202@users.noreply.github.com>
2025-04-05 17:54:57 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting ComfyUI..."
-cd /workspace/ComfyUI
-nohup python3 main.py > comfyui.log 2>&1 &
-
-echo "Starting Jupyter Lab..."
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password=''
